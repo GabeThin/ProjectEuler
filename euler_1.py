b96d657622660da1b6a758f13b14d512cbb929f2
Gabe Thin <gathin@packer.edu>
2021-05-24 11:23:59 -0400

    Euler 1

---
+++ euler_1.py
@@ -0,0 +1,17 @@
+def multiples(limit):
+    mult = 1
+    nums = []
+
+
+
+    while 5 * mult < limit or 3 * mult < limit:
+        if 3 * mult not in nums:
+            nums.append(3 * mult)
+        if 5 * mult < limit and 5 * mult not in nums:
+            nums.append(5 * mult)
+
+        mult += 1
+
+    return sum(nums)
+
+print(multiples(19564))
